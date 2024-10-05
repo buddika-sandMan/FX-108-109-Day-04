16f875b0551b58a93210e27bd304a2b496e2a461
Buddika <buddikab97@outlook.com>
2024-10-06 00:18:00 +0530

    use SQL transactions

--- src/main/java/controller/order/OrderController.java
+++ src/main/java/controller/order/OrderController.java
@@ -18,24 +18,30 @@ public class OrderController {
 
     public boolean placeOrder(Order order) throws SQLException {
         Connection connection = DBConnection.getInstance().getConnection();
-        String SQL = "INSERT INTO orders VALUES(?,?,?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(SQL);
-        preparedStatement.setObject(1,order.getOrderID());
-        preparedStatement.setObject(2,order.getDate());
-        preparedStatement.setObject(3,order.getCustomerID());
-        boolean isOrderAdded = preparedStatement.executeUpdate() > 0;
+        try {
+            connection.setAutoCommit(false);
+            String SQL = "INSERT INTO orders VALUES(?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            preparedStatement.setObject(1,order.getOrderID());
+            preparedStatement.setObject(2,order.getDate());
+            preparedStatement.setObject(3,order.getCustomerID());
+            boolean isOrderAdded = preparedStatement.executeUpdate() > 0;
 
-        if(isOrderAdded) {
-            boolean addedOrderDetails = new OrderDetailsController().addOrderDetails(order.getOrderDetailsList());
-            if (addedOrderDetails){
-                boolean isUpdatedStock = ItemController.getInstance().updateStock(order.getOrderDetailsList());
-                if (isUpdatedStock) {
-                    new Alert(Alert.AlertType.INFORMATION,"Oder placed.").show();
-                    return true;
+            if(isOrderAdded) {
+                boolean addedOrderDetails = new OrderDetailsController().addOrderDetails(order.getOrderDetailsList());
+                if (addedOrderDetails){
+                    boolean isUpdatedStock = ItemController.getInstance().updateStock(order.getOrderDetailsList());
+                    if (isUpdatedStock) {
+                        new Alert(Alert.AlertType.INFORMATION,"Oder placed.").show();
+                        return true;
+                    }
                 }
-            }
         }
-        new Alert(Alert.AlertType.ERROR,"Order NOT placed").show();
-        return false;
-         }
+            connection.rollback();
+            return false;
+
+        } finally {
+            connection.setAutoCommit(true);
+        }
     }
+}
